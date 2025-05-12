9d34bc485942d7e0da167fb4a7d8cdaa433a2cdb
Ahmed Essam <aahmedessam30@gmail.com>
2025-05-13 02:17:10 +0300

    fix: Update Facade Methods

--- src/Facades/RabbitMQ.php
+++ src/Facades/RabbitMQ.php
@@ -6,10 +6,16 @@ use AhmedEssam\LaraRabbit\Contracts\RabbitMQServiceInterface;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static bool publish(string $routingKey, array $data, array $properties = [])
+ * @method static bool publish(string $routingKey, array $data, array $options = [])
+ * @method static bool publishEvent(string $eventName, array $payload, array $options = [])
  * @method static bool publishBatch(array $messages)
- * @method static \AhmedEssam\LaraRabbit\Contracts\RabbitMQServiceInterface setupQueue(string $queueName, array $bindingKeys = [])
- * @method static void consume(string $queueName, callable $callback, array $bindingKeys = [])
+ * @method static bool validateMessage(array $data, string $schemaName)
+ * @method static \AhmedEssam\LaraRabbit\Contracts\RabbitMQServiceInterface setupQueue(string $queueName, array $bindingKeys = [], bool $durable = true, bool $autoDelete = false, array $arguments = [])
+ * @method static \AhmedEssam\LaraRabbit\Contracts\RabbitMQServiceInterface setupDeadLetterQueue(string $sourceQueue, string $deadLetterQueue, array $bindingKeys = [])
+ * @method static \AhmedEssam\LaraRabbit\Contracts\RabbitMQServiceInterface setupPredefinedQueue(string $queueKey)
+ * @method static \AhmedEssam\LaraRabbit\Services\RabbitMQService setSerializationFormat(string $format)
+ * @method static void consume(string $queueName, callable $callback, array $bindingKeys = [], bool $autoAck = false, array $arguments = [])
+ * @method static void consumeFromPredefinedQueue(string $queueKey, callable $callback, bool $autoAck = false, array $arguments = [])
  * @method static \PhpAmqpLib\Message\AMQPMessage|null getMessageFromQueue(string $queueName)
  * @method static void acknowledge(\PhpAmqpLib\Message\AMQPMessage $message)
  * @method static void reject(\PhpAmqpLib\Message\AMQPMessage $message, bool $requeue = false)
